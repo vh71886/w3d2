48054df23bec66b67fea9b352e13bfedaed2c10b
Tom Li <tomleslieli@yahoo.com>
2022-04-12 14:31:45 -0400

    added to board - up to place_cards

--- board.rb
+++ board.rb
@@ -1,20 +1,26 @@
+require_relative "./card.rb"
+
 class Board
+    attr_reader :array
 
     def initialize(n=4)
-        @board = Array.new(n) { Array.new( n, Card.new() ) }
+        @board = Array.new(n) { Array.new(n) }
         @length = n
         @array = []
     end
 
     def place_cards(length)
         (length**2 / 2).times do 
-            value = ("a".."z").upcase.to_a.sample
+            value = ("a".."z").to_a.sample.upcase
             pos = []
             count = 0
             while !@array.include?(pos) && count < 2
                 pos = random_pos(length)
-                @board[pos[0]][pos[1]] = value
+                @board[pos[0]][pos[1]] = Card.new(value)
                 count += 1
+                @array << pos
+                pos = []
+                # puts "count: #{count} / pos: #{pos} / value: #{value}"
             end
         end
 
@@ -24,8 +30,6 @@ class Board
         num1 = rand(0...length)
         num2 = rand(0...length)
         pos = [num1, num2]
-    end
-
-    
+    end 
 
 end
